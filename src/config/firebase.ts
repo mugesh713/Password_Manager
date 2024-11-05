3930e524c198a6e86855a2983849c92358541c3f
Mugesh V <125540370+mugesh713@users.noreply.github.com>
2024-11-05 14:06:38 +0530

    Your commit message

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+// Your firebase configuration goes here.
+//
+// Here is how you get your config:
+// - Open the Firebase console: https://console.firebase.google.com/
+// - Click on the Firebase project for Firepass.
+// - Click the settings icon (Project settings) in the sidebar
+//
+// See here for more information:
+// https://support.google.com/firebase/answer/7015592
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9gavlp9XWbms4PMxo4UxIPjNTSM7VDwE",
+  authDomain: "otpauthapp-1e20c.firebaseapp.com",
+  projectId: "otpauthapp-1e20c",
+  storageBucket: "otpauthapp-1e20c.firebasestorage.app",
+  messagingSenderId: "462479401294",
+  appId: "1:462479401294:web:1fc481a97340ca473f3e1f",
+  measurementId: "G-DQ6BFV1K3B"
+};
+
+
